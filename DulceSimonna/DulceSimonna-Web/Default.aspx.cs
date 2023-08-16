df7c737075f609a7f0fa87b5321d11fb273883b3
Diego <d.-romero@outlook.com>
2023-08-16 16:52:27 -0300

    Puesta en marcha del proyecto

---
+++ DulceSimonna/DulceSimonna-Web/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using dominio;
+using negocio;
+
+namespace DulceSimonna_Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public List<Articulo> listaArticulos;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ListadoNegocio negocio = new ListadoNegocio();
+            listaArticulos = negocio.listar();
+
+            if (!IsPostBack)
+            {
+                repetidor.DataSource = listaArticulos;
+                repetidor.DataBind();
+            }
+        }
+    }
+}
